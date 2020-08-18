82af0a00c5fd3faf2eb58f625b3e66837d10355a
Sean <sorelup@gmail.com>
2020-08-17 22:31:12 -0400

    Created node manager, fleshed out Node object, Created NodeManagerPreview

---
+++ Assets/NodeManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Spawns and provides access to all nodes in the level
+/// </summary>
+public class NodeManager : MonoBehaviour
+{
+    public int numRows = 0, numColumns = 0;
+    public float rowOffset, columnOffset;
+
+    /// <summary>
+    /// Has this script finished its startup?
+    /// </summary>
+    /// <value></value>
+    public bool isReady {get; private set;}
+
+    public NodeManager instance;
+    private Node[,] nodes;
+    public Node nodePrefab;
+    public GameObject nodeParent;
+    private Vector2Int playerBaseStart;
+    private Vector2Int enemyStart;
+    private Vector2Int[] enemyPath;
+    public NodeManagerPreview preview;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isReady = false;
+        instance = this;       
+        CopyPreview(preview);
+        CreateNodes();
+        isReady = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateNodes(){
+        nodes = new Node[numRows, numColumns];
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0; j < numColumns; j++){
+                nodes[i,j] = Instantiate(nodePrefab.gameObject, new Vector3(i * (1 + columnOffset),j * (1 + rowOffset),0), Quaternion.identity, nodeParent.transform).GetComponent<Node>();
+                Vector2 pos = new Vector2(i,j);
+
+                if(pos == playerBaseStart){
+                    nodes[i,j].nodeType = Node.NodeType.PLAYER_BASE;
+                }
+                else if(pos == enemyStart){
+                    nodes[i,j].nodeType = Node.NodeType.ENEMY_BASE;
+                }else{
+                    nodes[i,j].nodeType = Node.NodeType.PLAYER_TOWER;
+                    
+                    if(enemyPath != null){
+                        for(int z = 0; z < enemyPath.Length; z++){
+                            if(pos == enemyPath[z]){
+                                nodes[i,j].nodeType = Node.NodeType.ENEMY_PATH;
+                            }
+                        }
+                    }
+                }
+                nodes[i,j].ColorNode(); //DEBUG LINE
+            }
+        }
+    }
+
+    void CopyPreview(NodeManagerPreview preview){
+        if(preview != null){
+            numColumns = preview.numColumns;
+            numRows = preview.numColumns;
+            columnOffset = preview.spacing.x;
+            rowOffset = preview.spacing.y;
+            playerBaseStart = preview.playerBaseStart;
+            enemyStart = preview.enemyStart;
+            enemyPath = preview.enemyPath;
+        }
+    }
+}
---
+++ Assets/NodeManagerPreview.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for showing a preview of node locations before game startup
+/// </summary>
+[ExecuteInEditMode]
+public class NodeManagerPreview : MonoBehaviour
+{
+    public int numRows = 0, numColumns = 0;
+    public float sphearSize = 1f;
+    public bool drawLines = true;
+
+    public Vector2 spacing;
+    public Vector2Int playerBaseStart;
+    public Vector2Int enemyStart;
+    public Vector2Int[] enemyPath;
+
+
+   // Start is called before the first frame update
+    /// <summary>
+    /// Callback to draw gizmos only if the object is selected.
+    /// </summary>
+    void OnDrawGizmosSelected()
+    {
+        //draw sphears
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0; j < numColumns; j++){
+                Vector2 pos = new Vector2(i,j);
+
+                if(pos == playerBaseStart){
+                    Gizmos.color = Node.PLAYER_BASE_COLOR;
+                }
+                else if(pos == enemyStart){
+                    Gizmos.color = Node.ENEMY_BASE_COLOR;
+                }else{
+                    Gizmos.color = Node.PLAYER_TOWER_COLOR;
+                    
+                    if(enemyPath != null){
+                        for(int z = 0; z < enemyPath.Length; z++){
+                            if(pos == enemyPath[z]){
+                                Gizmos.color = Node.ENEMY_PATH_COLOR;
+                            }
+                        }
+                    }
+                }
+                 Gizmos.DrawSphere(new Vector3(i * (1 + spacing.x),j * (1 + spacing.y),0),sphearSize);
+            }
+        }
+
+        //draw enemy path
+        if(drawLines){
+            Gizmos.color = Node.ENEMY_PATH_COLOR;
+            for(int i = 0; i < enemyPath.Length; i++){
+                if(i == 0){
+                    Gizmos.DrawLine((Vector3Int)enemyStart, (Vector3Int)enemyPath[i]);
+                    if(i < enemyPath.Length - 1){
+                        Gizmos.DrawLine((Vector3Int)enemyPath[i], (Vector3Int)enemyPath[i + 1]);
+                    }
+                }else if(i < enemyPath.Length - 1){
+                    Gizmos.DrawLine((Vector3Int)enemyPath[i], (Vector3Int)enemyPath[i + 1]);
+                }else{
+                    Gizmos.DrawLine((Vector3Int)enemyPath[i], (Vector3Int)playerBaseStart);
+                }
+            }
+        }
+       
+    }
+}
